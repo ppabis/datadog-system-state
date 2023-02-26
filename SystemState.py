ed76ee13b5214c0e29bf435c537491ad370a2eca
ppabis <piotr@pabis.eu>
2023-02-26 15:12:41 +0100

    OS major version

--- SystemState.py
+++ SystemState.py
@@ -30,5 +30,28 @@ class SystemState(AgentCheck):
         boot_time = datetime.strptime(out, "%Y-%m-%d %H:%M:%S")
         return (datetime.now() - boot_time).days
 
+    def get_os_major_version(self):
+        version = "-1"
+        # Using lsb_release binary if present
+        if shutil.which("lsb_release") is not None:
+            out, _, _ = get_subprocess_output(["lsb_release", "-rs"], self.log)
+            version = out
+        # Using /etc/lsb-release file if present
+        elif os.path.isfile("/etc/lsb-release"):
+            with open("/etc/lsb-release", "r") as f:
+                for line in f:
+                    if line.startswith("DISTRIB_RELEASE="):
+                        version = line.split("=")[1]
+                        break
+        # Using /etc/os-release file if present
+        elif os.path.isfile("/etc/os-release"):
+            with open("/etc/os-release", "r") as f:
+                for line in f:
+                    if line.startswith("VERSION_ID="):
+                        version = line.split("=")[1]
+                        break
+        
+        return int( version.strip('"').split(".")[0] )
+
     def check(self, instance):
         pass
